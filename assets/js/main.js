8655e9edc0d410c84c428fed58760001ff7d3995
bidi <bidixprojects@gmail.com>
2025-11-12 05:12:21 +0200

    0.0.7
    Cleaned ui.

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,7 +121,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // If current layout is vertical and current window screen is > small
 
   // Auto update menu collapsed/expanded based on the themeConfig
-      window.Helpers.setCollapsed(true, false);
+  window.Helpers.setCollapsed(true, false);
 })();
 // Utils
 function isMacOS() {
